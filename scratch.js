763b0a9e56acc19dfe14e29526565311da9aadf5
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2024-08-17 16:04:50 -0700

    made scratch file - error msg: listen EADDRINUSE: address already in use :::5001

---
+++ scratch.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+
+
+
+})
+
+const port = 5001;
+
+
+server.listen(port, () => console.log('Server is listening on port', port));
